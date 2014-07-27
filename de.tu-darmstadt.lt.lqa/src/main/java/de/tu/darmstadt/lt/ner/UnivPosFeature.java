7b6757a04a48d24b2abb62414098a730a378b77c
seidymam <seidymam@797bc98d-c7dc-a395-f672-6b0218ecb56f>
2014-07-27 14:18:25 +0000

    making structural changes

---
+++ de.tu-darmstadt.lt.lqa/src/main/java/de/tu/darmstadt/lt/ner/UnivPosFeature.java
@@ -0,0 +1,9 @@
+package de.tu.darmstadt.lt.ner;
+
+import java.util.LinkedList;
+
+public class UnivPosFeature {
+	
+	public static LinkedList<String> pos=new LinkedList<String>();
+
+}
---
+++ de.tu-darmstadt.lt.lqa/src/main/java/de/tu/darmstadt/lt/ner/types/SimilarWord2_Type.java
@@ -0,0 +1,93 @@
+
+/* First created by JCasGen Sun Jul 20 17:09:52 CEST 2014 */
+package de.tu.darmstadt.lt.ner.types;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Sun Jul 20 17:09:52 CEST 2014
+ * @generated */
+public class SimilarWord2_Type extends Annotation_Type {
+  /** @generated 
+   * @return the generator for this type
+   */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /** @generated */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (SimilarWord2_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = SimilarWord2_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new SimilarWord2(addr, SimilarWord2_Type.this);
+  			   SimilarWord2_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new SimilarWord2(addr, SimilarWord2_Type.this);
+  	  }
+    };
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = SimilarWord2.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("de.tu.darmstadt.lt.lqa.webapp.types.SimilarWord2");
+ 
+  /** @generated */
+  final Feature casFeat_Value;
+  /** @generated */
+  final int     casFeatCode_Value;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getValue(int addr) {
+        if (featOkTst && casFeat_Value == null)
+      jcas.throwFeatMissing("Value", "de.tu.darmstadt.lt.lqa.webapp.types.SimilarWord2");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_Value);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setValue(int addr, String v) {
+        if (featOkTst && casFeat_Value == null)
+      jcas.throwFeatMissing("Value", "de.tu.darmstadt.lt.lqa.webapp.types.SimilarWord2");
+    ll_cas.ll_setStringValue(addr, casFeatCode_Value, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public SimilarWord2_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_Value = jcas.getRequiredFeatureDE(casType, "Value", "uima.cas.String", featOkTst);
+    casFeatCode_Value  = (null == casFeat_Value) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_Value).getCode();
+
+  }
+}
+
+
+
+    
