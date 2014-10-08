1951b4cb6d562193e98b5ee8e4536269a8ce8da4
seidymam <seidymam@797bc98d-c7dc-a395-f672-6b0218ecb56f>
2014-10-08 10:23:48 +0000

    separate sentence with a new line in a test file result

--- trunk/germanner/src/main/java/de/tu/darmstadt/lt/ner/writer/EvaluatedNERWriter.java
+++ trunk/germanner/src/main/java/de/tu/darmstadt/lt/ner/writer/EvaluatedNERWriter.java
@@ -13,43 +13,49 @@ import org.uimafit.util.JCasUtil;
 
 import de.tu.darmstadt.lt.ner.types.GoldNamedEntity;
 import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 
-public class EvaluatedNERWriter extends JCasConsumer_ImplBase
+public class EvaluatedNERWriter
+    extends JCasConsumer_ImplBase
 {
-	public static final String OUTPUT_FILE = "OutputFile";
+    public static final String OUTPUT_FILE = "OutputFile";
 
-	@ConfigurationParameter(name = OUTPUT_FILE, mandatory = true)
-	private File OutputFile = null;
-	
-	public static final String LF = System.getProperty("line.separator");
+    @ConfigurationParameter(name = OUTPUT_FILE, mandatory = true)
+    private File OutputFile = null;
 
-	public void process(JCas jcas) throws AnalysisEngineProcessException
-	{
-		try
-		{
-			FileWriter fw = new FileWriter(OutputFile);
-			
-			StringBuilder sb;
+    public static final String LF = System.getProperty("line.separator");
 
-			for (NamedEntity a : JCasUtil.select(jcas, NamedEntity.class))
-			{
-				sb = new StringBuilder();
-				sb.append(a.getCoveredText().replace(" ", ""));
-				sb.append(" ");
-				sb.append(JCasUtil.selectCovered(jcas, GoldNamedEntity.class, a).get(0).getNamedEntityType());
-				sb.append(" ");
-				sb.append(a.getValue());
-				sb.append(LF);
-				fw.write(sb.toString());
-			}
-			
-			fw.close();
-			
-			getContext().getLogger().log(Level.INFO, "Output written to: " + OutputFile.getAbsolutePath());
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void process(JCas jcas)
+        throws AnalysisEngineProcessException
+    {
+        try {
+            FileWriter fw = new FileWriter(OutputFile);
+
+            StringBuilder sb;
+            for (Sentence sentence : JCasUtil.select(jcas, Sentence.class)) {
+                for (NamedEntity a : JCasUtil.selectCovered(jcas, NamedEntity.class,
+                        sentence.getBegin(), sentence.getEnd())) {
+                    sb = new StringBuilder();
+                    sb.append(a.getCoveredText().replace(" ", ""));
+                    sb.append(" ");
+                    sb.append(JCasUtil.selectCovered(jcas, GoldNamedEntity.class, a).get(0)
+                            .getNamedEntityType());
+                    sb.append(" ");
+                    sb.append(a.getValue());
+                    sb.append(LF);
+                    fw.write(sb.toString());
+                }
+                fw.write(LF);
+
+            }
+            fw.close();
+
+            getContext().getLogger().log(Level.INFO,
+                    "Output written to: " + OutputFile.getAbsolutePath());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
