ae2a7ed9df88d42ae8cc7e888044c4e4d41ebcdb
Seid Muhie Yimam <seidymam@gmail.com>
2015-08-03 15:28:16 +0200

    remove unused method

--- germanner/src/main/java/de/tu/darmstadt/lt/ner/reader/NERReader.java
+++ germanner/src/main/java/de/tu/darmstadt/lt/ner/reader/NERReader.java
@@ -293,44 +293,6 @@ public class NERReader
         reader.close();
     }
 
-    private void dbPersonNameFileToMap(String fileName)
-        throws Exception
-    {
-
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            try {
-                String[] st = line.split("\t");
-                freebaseMap.put(st[0], "Person");
-            }
-            catch (Exception e) {
-                System.out.println("Check if the freebase person name list file is correct "
-                        + e.getMessage());
-            }
-        }
-        reader.close();
-    }
-
-    private void dbLocationNameFileToMap(String fileName)
-        throws Exception
-    {
-
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            try {
-                String[] st = line.split("\t");
-                freebaseMap.put(st[0], "Location");
-            }
-            catch (Exception e) {
-                System.out.println("Check if the freebase person name list file is correct "
-                        + e.getMessage());
-            }
-        }
-        reader.close();
-    }
-
     private void suffixClassToMap(String fileName)
         throws Exception
     {
